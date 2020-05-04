c19350a3887fe44eb14c410e51eddb5e14558401
wenner <wennersannerma@gmail.com>
2020-05-03 21:06:50 -0300

    #25 - Foi implementada a funcionalidade para evitar duplicidade de compras de um mesmo produto

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -19,8 +19,23 @@ class CartController extends Controller
 
         //existe sessao para os produtos?
         if (session()->has('cart')) {
-            //sim: atualizar os produtos na sessão existente
-            session()->push('cart', $produto);
+
+            $produtos = session()->get('cart');
+//            dd($produtos);
+            $produtosSlugs = array_column($produtos, 'slug');
+
+            //evitando duplicidades
+            //se o slug já existir, se a sessãpjá tiver umproduto com o mesmo slug de $produtosSlugs
+            if (in_array($produto['slug'], $produtosSlugs)) {
+                $produtos = $this->incrementaProduto($produto['slug'], $produto['quantidade'], $produtos);
+                session()->put('cart', $produtos);
+
+                //se não tiver duplicidades
+            } else {
+                //sim: atualizar os produtos na sessão existente
+                session()->push('cart', $produto);
+            }
+
 
         } else {
             //não: criar sessão com o primeiro produto
@@ -54,5 +69,17 @@ class CartController extends Controller
         session()->forget('cart');
         flash('Desistencia da compra realizada com sucesso')->success();
         return redirect()->route('cart.index');
+    }
+
+    private function incrementaProduto($slug, $quantidade, $produtos)
+    {
+        $produtos = array_map(function($line) use ($slug, $quantidade){
+            if ($slug == $line['slug']) {
+                $line['quantidade'] += $quantidade;
+            }
+            return $line;
+        }, $produtos);
+
+        return $produtos;
     }
 }
