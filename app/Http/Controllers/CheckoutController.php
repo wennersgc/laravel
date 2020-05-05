5d3b90bf9a8cca7151c53f876040c77cb23a4bc7
wenner <wennersannerma@gmail.com>
2020-05-05 18:43:41 -0300

    #28 - Foi criada a session code pagseguro para cada compra

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -12,6 +12,20 @@ class CheckoutController extends Controller
             return redirect()->route('login');
         }
 
+        $this->makePagSeguroSession();
+
         return view('checkout');
+    }
+
+    private function makePagSeguroSession()
+    {
+         if (!session()->has('pagseguro_session_code')) {
+             $sessionCode = \PagSeguro\Services\Session::create(
+                 \PagSeguro\Configuration\Configure::getAccountCredentials()
+             );
+//             dd( $sessionCode->getResult());
+            session()->put('pagseguro_session_code', $sessionCode->getResult());
+         }
+
     }
 }
