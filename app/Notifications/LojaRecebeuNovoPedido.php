db3f610f04f8b9c67e3502fc0e3a9e547fe989da
wenner <wennersannerma@gmail.com>
2020-05-11 12:53:57 -0300

    #49 - Foi implementado a notificação via email para o dono da loja quand houver um novo pedido

--- app/Notifications/LojaRecebeuNovoPedido.php
+++ app/Notifications/LojaRecebeuNovoPedido.php
@@ -29,7 +29,7 @@ class LojaRecebeuNovoPedido extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
@@ -41,9 +41,10 @@ class LojaRecebeuNovoPedido extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->subject('')
+                    ->greeting('Olá vendedor, tudo bem?')
+                    ->line('Você recebeu um novo pedido na loja')
+                    ->action('Ver pedido', route('admin.orders.my'));
     }
 
     /**
